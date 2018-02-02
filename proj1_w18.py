24bc693d064f320493a47f572926f5493573940e
jonhartm <jonhartm@umich.edu>
2018-02-02 12:23:18 -0500

    Initial Commit

---
+++ proj1_w18.py
@@ -0,0 +1,69 @@
+import requests
+import json
+
+class Media:
+
+    def __init__(self, title="No Title", author="No Author", release="No Year", json=None):
+        if json is None:
+            self.title = title
+            self.author = author
+            self.release = release
+        else:
+            self.title = json["collectionName"]
+            self.author = json["artistName"]
+            self.release = int(json["releaseDate"][:4])
+
+    def __str__(self):
+        return "{} by {} ({})".format(self.title, self.author, self.release)
+
+    def __len__(self):
+        return 0
+
+## Other classes, functions, etc. should go here
+
+class Song(Media):
+    def __init__(self, title="No Title", author="No Author", release="No Year", album="No Album", genre="No Genre", track_length=0, json=None):
+        if json is None:
+            super().__init__(title, author, release, json=json)
+            self.album = album
+            self.genre = genre
+            self.track_length = track_length
+        else:
+            super().__init__(json=json)
+            self.title = json["trackName"]
+            self.album = json["collectionName"]
+            self.genre = json["primaryGenreName"]
+            self.track_length = round(int(json["trackTimeMillis"]) / 1000)
+
+    def __str__(self):
+        return super().__str__() + " [{}]".format(self.genre)
+
+    def __len__(self):
+        return self.track_length
+
+class Movie(Media):
+    def __init__(self, title="No Title", author="No Author", release="No Year", rating="No Rating", movie_length=0, json=None):
+        if json is None:
+            super().__init__(title, author, release)
+            self.rating = rating
+            self.movie_length = movie_length
+        else:
+            super().__init__(json=json)
+            self.title = json["trackName"]
+            self.rating = json["contentAdvisoryRating"]
+            self.movie_length = round(int(json["trackTimeMillis"])/1000/60)
+
+    def __str__(self):
+        return super().__str__() + " [{}]".format(self.rating)
+
+    def __len__(self):
+        return self.movie_length
+
+def getiTunesData(search_term):
+    baseurl = "https://itunes.apple.com/search"
+    dict_params = {'term' : search_term}
+    return requests.get(baseurl, dict_params).json()["results"]
+
+if __name__ == "__main__":
+    	# your control code for Part 4 (interactive search) should go here
+        pass
---
+++ proj1_w18_test.py
@@ -0,0 +1,107 @@
+import unittest
+import json
+import proj1_w18 as proj1
+
+class TestMedia(unittest.TestCase):
+    def setUp(self):
+        self.test_media = proj1.Media("Bridget Jones's Diary (Unabridged)", "Helen Fielding", 2012)
+
+    def testConstructor(self):
+        m1 = proj1.Media()
+        m2 = proj1.Media("1999", "Prince")
+
+        self.assertEqual(m1.title, "No Title")
+        self.assertEqual(m1.author, "No Author")
+        self.assertEqual(m2.title, "1999")
+        self.assertEqual(m2.author, "Prince")
+        self.assertEqual(self.test_media.release, 2012)
+
+    def test_string(self):
+        self.assertEqual(str(self.test_media), "Bridget Jones's Diary (Unabridged) by Helen Fielding (2012)")
+
+    def test_len(self):
+        self.assertEqual(len(self.test_media), 0)
+
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        self.test_song = proj1.Song(title="Hey Jude", author="The Beatles", album="Hey Jude (Single)", release=1968, genre="Rock", track_length=431)
+
+    def testConstructor(self):
+        s1 = proj1.Song()
+        s2 = proj1.Song("Physical", "Olivia Newton-John", release=1981, genre="Dance")
+
+        self.assertEqual(s1.title, "No Title")
+        self.assertEqual(s1.author, "No Author")
+        self.assertEqual(s1.album, "No Album")
+        self.assertEqual(s1.release, "No Year")
+        self.assertEqual(s1.genre, "No Genre")
+        self.assertEqual(s2.title, "Physical")
+        self.assertEqual(s2.author, "Olivia Newton-John")
+        self.assertEqual(s2.release, 1981)
+        self.assertEqual(s2.genre, "Dance")
+
+    def test_string(self):
+        self.assertEqual(str(self.test_song), "Hey Jude by The Beatles (1968) [Rock]")
+
+    def test_len(self):
+        self.assertEqual(len(self.test_song), 431)
+
+class TestMovie(unittest.TestCase):
+    def setUp(self):
+        self.test_movie = proj1.Movie(title="Jaws", author="Steven Speilberg", release="1975", rating="PG", movie_length=124)
+
+    def testConstructor(self):
+        self.assertEqual(self.test_movie.rating, "PG")
+        self.assertEqual(self.test_movie.movie_length, 124)
+
+    def test_string(self):
+        self.assertEqual(str(self.test_movie), "Jaws by Steven Speilberg (1975) [PG]")
+
+    def test_len(self):
+        self.assertEqual(self.test_movie.movie_length, 124)
+
+class TestJSONConstructors(unittest.TestCase):
+    def setUp(self):
+        with open("sample_json.json", 'r') as file:
+            jsonfile = json.load(file)
+
+        for entry in jsonfile:
+            if not "kind" in entry: # kind only appears on songs or movies?
+                self.media_test = proj1.Media(json=entry)
+            elif entry["kind"] == "song":
+                self.song_test = proj1.Song(json=entry)
+            elif entry["kind"] == "feature-movie":
+                self.movie_test = proj1.Movie(json=entry)
+
+    def test_Constructors(self):
+        self.assertEqual(self.media_test.title, "Bridget Jones's Diary (Unabridged)")
+        self.assertEqual(self.media_test.author, "Helen Fielding")
+        self.assertEqual(self.media_test.release, 2012)
+
+        self.assertEqual(self.song_test.title, "Hey Jude")
+        self.assertEqual(self.song_test.author, "The Beatles")
+        self.assertEqual(self.song_test.release, 1968)
+        self.assertEqual(self.song_test.album, "TheBeatles 1967-1970 (The Blue Album)")
+        self.assertEqual(self.song_test.genre, "Rock")
+
+        self.assertEqual(self.movie_test.title, "Jaws")
+        self.assertEqual(self.movie_test.author, "Steven Spielberg")
+        self.assertEqual(self.movie_test.release, 1975)
+        self.assertEqual(self.movie_test.rating, "PG")
+
+    def test_string(self):
+        self.assertEqual(str(self.media_test), "Bridget Jones's Diary (Unabridged) by Helen Fielding (2012)")
+        self.assertEqual(str(self.song_test), "Hey Jude by The Beatles (1968) [Rock]")
+        self.assertEqual(str(self.movie_test), "Jaws by Steven Spielberg (1975) [PG]")
+
+    def test_len(self):
+        self.assertEqual(len(self.media_test), 0)
+        self.assertEqual(len(self.song_test), 431)
+        self.assertEqual(len(self.movie_test), 124)
+
+class TestiTunesAPI(unittest.TestCase):
+    def test_commonWords(self):
+        for result in proj1.getiTunesData("baby"):
+            print(proj1.Media(json=result))
+
+unittest.main()
